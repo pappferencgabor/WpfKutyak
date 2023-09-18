0e193b9fbbdc22d326e957fbbddbef5ed1eb2129
pappferencgabor <70635774+pappferencgabor@users.noreply.github.com>
2023-09-18 19:41:43 +0200

    feltoltes

---
+++ WpfKutyak/Kutya.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfKutyak
+{
+    internal class Kutya
+    {
+        public int ID { get; set; }
+        public int FajID { get; set; }
+        public int NevID { get; set; }
+        public int Kor { get; set; }
+        public string UtolsoOrvos { get; set; }
+
+        public Kutya(string sor)
+        {
+            ID = int.Parse(sor.Split(";")[0]);
+            FajID = int.Parse(sor.Split(";")[1]);
+            NevID = int.Parse(sor.Split(";")[2]);
+            Kor = int.Parse(sor.Split(";")[3]);
+            UtolsoOrvos = sor.Split(";")[4];
+        }
+    }
+
+    internal class KutyaNevek
+    {
+        public int ID { get; set; }
+        public string Nev { get; set; }
+
+        public KutyaNevek(string sor)
+        {
+            ID = int.Parse(sor.Split(";")[0]);
+            Nev = sor.Split(";")[1];
+        }
+    }
+
+    internal class KutyaFajtak
+    {
+        public int ID { get; set; }
+        public string Nev { get; set; }
+        public string EredetiNev { get; set; }
+
+        public KutyaFajtak(string sor)
+        {
+            ID = int.Parse(sor.Split(";")[0]);
+            Nev = sor.Split(";")[1];
+            EredetiNev = sor.Split(";")[2];
+        }
+    }
+}
---
+++ WpfKutyak/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfKutyak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static List<KutyaNevek> kutyaNevek = new List<KutyaNevek>();
+        static List<KutyaFajtak> kutyaFajtak = new List<KutyaFajtak>();
+        static List<Kutya> kutyak = new List<Kutya>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            kutyaNevek = File.ReadAllLines("Adatok\\KutyaNevek.csv").Skip(1).Select(x => new KutyaNevek(x)).ToList();
+            kutyaFajtak = File.ReadAllLines("Adatok\\KutyaFajtak.csv").Skip(1).Select(x => new KutyaFajtak(x)).ToList();
+            kutyak = File.ReadAllLines("Adatok\\Kutyak.csv").Skip(1).Select(x => new Kutya(x)).ToList();
+        }
+
+        private void btnFeladat3_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"3. feladat: Kutyanevek száma: {kutyaNevek.Count}");
+        }
+
+        private void btnFeladat6_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"6. feladat: Kutyák átlag életkora: {Math.Round(kutyak.Average(x => x.Kor), 2)}");
+        }
+
+        private void btnFeladat7_Click(object sender, RoutedEventArgs e)
+        {
+            Kutya legidosebb = kutyak.OrderByDescending(x => x.Kor).ToList()[0];
+            string legidosebbNev = "";
+            string legidosebbFaj = "";
+
+            foreach (var item in kutyaNevek)
+            {
+                if (item.ID == legidosebb.NevID)
+                {
+                    legidosebbNev = item.Nev;
+                }
+            }
+            foreach (var item in kutyaFajtak)
+            {
+                if (item.ID == legidosebb.FajID)
+                {
+                    legidosebbFaj = item.Nev;
+                }
+            }
+
+            MessageBox.Show($"7. feladat: Legidősebb kutya neve és fajtája: {legidosebbNev}, {legidosebbFaj}");
+        }
+
+        private void btnFeladat8_Click(object sender, RoutedEventArgs e)
+        {
+            string vizsgaltKutyakStr = "";
+            List<Kutya> vizsgaltKutyak = kutyak.Where(x => x.UtolsoOrvos == "2018.01.10").ToList();
+
+            foreach (Kutya kutya in vizsgaltKutyak)
+            {
+                foreach (KutyaFajtak kutyaFajta in kutyaFajtak)
+                {
+                    if (kutyaFajta.ID == kutya.FajID)
+                    {
+                        vizsgaltKutyakStr += $"{kutyaFajta.Nev}: 1 kutya\n"; // nem tudom hogy kell hogy hány kutya van
+                        break;
+                    }
+                }
+            }
+
+            MessageBox.Show($"8. feladat: Január 10.-én vizsgált kutya fajták:\n{vizsgaltKutyakStr}");
+        }
+
+        private void btnFeladat9_Click(object sender, RoutedEventArgs e)
+        {
+            var legterheltebbNap = kutyak.OrderByDescending(x => x.UtolsoOrvos).ToList()[0];
+            MessageBox.Show($"9. feladat: Legjobban leterhelt nap: {legterheltebbNap}");
+        }
+
+        private void btnFeladat10_Click(object sender, RoutedEventArgs e)
+        {
+            Dictionary<string, int> kutyaNevekDict = new Dictionary<string, int>();
+
+            foreach (Kutya kutya in kutyak)
+            {
+                string nev = kutyaNevek.Where(x => x.ID == kutya.NevID).ToList()[0].Nev;
+
+                if (kutyaNevekDict.ContainsKey(nev))
+                {
+                    kutyaNevekDict[nev] ++;
+                }
+                else
+                {
+                    kutyaNevekDict.Add(nev, 1);
+                }
+            }
+        }
+    }
+}
